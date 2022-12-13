f380d7ea0de067b8f3f8a83e8ad358ee37faf5c1
Umar12369 <Omar_aizan@hotmail.com>
2022-12-12 22:38:38 -0500

    create the while loop and create movie 1 and csv

--- Application.py
+++ Application.py
@@ -1,7 +1,27 @@
+from Movies import Movie
+from Movies import MovieStore
 
-def print_menu():
+def print_menu(MovieStore):
     print("1. Create movie")
     print("2. Read movie")
     print("3. Update movie")
     print("4. Delete movie")
     print("5. Quit")
+
+if __name__ == "__main__":
+    movie_store = MovieStore()
+    choice = None
+
+    while True:
+        print_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            title = input("Enter movie title: ")
+            director = input("Enter movie director: ")
+            release_year = input("Enter movie release year: ")
+            movie = Movie(title, director, release_year)
+            movie_store.add_movie(movie)
+            print("Movie added!")
+            # Write the movies to the CSV file
+            movie_store.write_to_csv("movies.csv")
