c8d5c39214b03234ddb7a2cc8e17b2774658f64c
Umar12369 <Omar_aizan@hotmail.com>
2022-12-12 22:26:46 -0500

    created the class movie and the csv format

--- Movies.py
+++ Movies.py
@@ -9,3 +9,23 @@ class Movie:
     def __str__(self):
         return f"{self.title} ({self.release_year}) - directed by {self.director}" 
         #return method for the each of the class self attributes or data
+
+class MovieStore:
+    def __init__(self):
+        #list of movies
+        self.movies = []
+
+    def add_movie(self, movie):
+        # create movie to the list of movies
+        self.movies.append(movie)
+
+    def write_to_csv(self, filename):
+        # Create a new file with the given filename
+        with open(filename, "w", newline="") as csvfile:
+            # Use the csv.writer() method to create a writer object
+            writer = csv.writer(csvfile)
+            # Write the column headers
+            writer.writerow(["Title", "Director", "Release Year"])
+            # Iterate over the movies in the store and write each one to the file
+            for movie in self.movies:
+                writer.writerow([movie.title, movie.director, movie.release_year])
